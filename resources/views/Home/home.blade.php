f1c72f5d71a1fef80047f62e343cb75f276d89ea
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-01-19 11:24:23 +0100

    Add link

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -3,6 +3,6 @@
     <title>Clubbers-Home</title>
     <body>
         <h1>HOME</h1>
-        <img src="{{route('Image.get', 2)}}">
+        <h2><a href="/create-user">REGISTRAZIONE</a></h2>
     </body>
 </html>
