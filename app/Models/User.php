f74a931e409add820ebef89b49004acd0c00f4fa
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-01-23 16:21:57 +0100

    modified User Class

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,31 @@
 
 namespace App\Models;
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    protected $fillable = [
+        'name', 
+        'surname',
+        'birth', 
+        'city',
+        'email', 
+        'phone', 
+        'password', 
+        'username',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+
 }
