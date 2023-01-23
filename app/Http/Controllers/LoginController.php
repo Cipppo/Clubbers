f7cf2122bc05f1d51685a51e0606cd97177cb8ab
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-01-23 16:22:34 +0100

    Implemented Logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,17 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     public function create(){
         return view('User.logIn');
     }
-
+ 
     public function authenticate(Request $request){
-        $credentials = $request->only('email', 'password');
+        //dd($request);
+
+        $messages = [
+            'email' => 'Invalid email or password',
+            'password' => 'Invalid email or password',
+        ];
+
+        $credentials = $request->validate([
+            'email' => ['required','email'],
+            'password' => ['required', 'String'],
+        ], $messages);
+        
         if(Auth::attempt($credentials)){
-            return redirect()->intended('dashboard');
+            //$request->session()->regenerate();
+            return redirect()->route('Feed.Home');
         }
+    }
+
+    public function logOut(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
