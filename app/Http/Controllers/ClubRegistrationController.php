654b19149ddf3afbdb170f1a1aba76263bb93367
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-01-25 15:11:39 +0100

    Implemented store and store image

---
+++ app/Http/Controllers/ClubRegistrationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Club;
+
+class ClubRegistrationController extends Controller
+{
+    
+    
+
+    public function create(){
+        return view('Club.clubRegistration');
+    }
+
+    public function store(Request $request){
+        $club = Club::create([
+            'name' => $request->name, 
+            'email' => $request->email, 
+            'phone' => $request->phone, 
+            'password' => bcrypt($request->password), 
+            'via' => $request->street,
+            'CAP' => $request->cap, 
+            'comune' => $request->city, 
+            'regione' => $request->state,
+        ]);
+
+
+        $this->storeImage($request);
+        //Store image
+        //Save 
+        //Redirect
+
+        //$club->save();
+
+        return redirect()->route("Home.home"); //Needs to be changed with the Feed Home
+    
+
+    }   
+
+    public function storeImage(Request $request){
+        $IMAGE_UPLOAD_URL='public/uploads/imgs/proPics';
+
+        $file = $request->hasFile('choose-file');
+        if($file){
+            $newFile = $request->file('choose-file');
+            $newFile->storeAs($IMAGE_UPLOAD_URL, $request->name."_propic.png");        
+        }
+    }
+}
