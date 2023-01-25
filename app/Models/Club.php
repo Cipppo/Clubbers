2d0a228fbfdba9b2b0156921fcda699d46a5d778
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-01-25 15:10:45 +0100

    Update Club Model

--- app/Models/Club.php
+++ app/Models/Club.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Club extends Model
+class Club extends Authenticatable
 {
     use HasFactory;
+
+    public $timestamps = false;
+    protected $table = 'club';
+
+    protected $fillable = [
+        'name', 
+        'email', 
+        'phone', 
+        'password',
+        'via', 
+        'CAP', 
+        'comune', 
+        'regione',
+    ];
+    
+    protected $hidden = [
+        'password',
+    ];
+
 }
