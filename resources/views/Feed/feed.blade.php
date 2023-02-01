479bea62b3e248c5e7b45aa21935f0b13a94c1c2
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-02-01 22:17:00 +0100

    Modified feed page in order to try the like function

--- resources/views/Feed/feed.blade.php
+++ resources/views/Feed/feed.blade.php
@@ -5,42 +5,25 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Post</title>
-        @vite(['../resources/css/userPost.css'])
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        @vite(['../resources/css/userPost.css', '../resources/js/Like/Like.js',  '../resources/js/app.js'])
     </head>
     <h1>Home Feed</h1>
-    <h1>{{ Auth::user()->id}}</h1>
+    <h1>{{ Auth::user()->username}}</h1>
     @php
-        $posts = App\Http\Controllers\postClubberController::getAll('Cipe');  
+        $posts = App\Http\Controllers\postClubberController::getAll();  
     @endphp
 
 
     @foreach ($posts as $post)
-        <h2>{{$post->caption}}</h2>
+        <h1>-----</h1>
+        <h1>{{$post->clubberUsername}} ha postato</h1>
+        <h3><strong>{{$post->caption}}</strong> al {{$post->clubUsername}}</h3>
+        <button id="like-button" name="{{$post->id}}">METTI LIKE</button>
     @endforeach
 
-
-    <body>
-        <div class="w-21 items-center text-slate-200 py-2">
-            <div class="post-User rounded-xl bg-black bg-opacity-50 backdrop-blur">
-                <div class ="post-banner rounded-t-lg">
-                    <img class ="rounded-t-lg w-full" src="{{'images/Banner1.jpg'}}" alt="banner">
-                </div>
-                <div class="post-Profile flex items-center gap-2 p-2">
-                    <img class="post-profilePicture object-fill h-20 w-20  rounded-full" src="?????" alt="profile picture">
-                    <a class="post-Username" href="">?????</a>
-                    <a class="post-clubTag rounded-full bg-black p-0.5 px-1 opacity-30" href="">?????</a>
-                </div>
-                <div class="post-info p-2">
-                    <p class="post-caption">?????</p>
-                </div>
-                <div class="post-buttons flex gap-2 pb-5">
-                    <a href=""><img class="h-5 w-5" src="img/like-not-pressed.png" alt="like"></a>
-                    <a href=""><img class="h-5 w-5" src="img/comment.png" alt="comment"></a>
-                    <a href=""><img class="h-5 w-5" src="img/tagged-people.png" alt="tagged people"></a>
-                </div>
-            </div>
-        </div>
-    </body>
+    <button id="prova">PROVA</button>
+    @csrf
 
 
 </html>
