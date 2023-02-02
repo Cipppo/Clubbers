8eb411c53b0475783bde6d0cb3507d92c24a851b
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-02-02 16:13:37 +0100

    Add tags

--- resources/views/Feed/feed.blade.php
+++ resources/views/Feed/feed.blade.php
@@ -30,11 +30,7 @@
             </div>            
     </nav>
 </header>
-<<<<<<< HEAD
 <body class="bg-fixed object-fill" style="background-image: url(images/feed/background.jpg)">
-=======
-<body class="bg-fixed" style="background-image: url(images/feed/background.jpg)">
->>>>>>> ce362b7 (add dinamic tags)
 <div class="py-[5%] lg:grid grid-cols-3 justify-between text-slate-200" >
     <div class="left"></div>
 
@@ -49,21 +45,12 @@
         <div class="w-21 items-center text-slate-200 py-2">
             <div class="post-User rounded-xl bg-black bg-opacity-50 backdrop-blur">
                 <div class ="post-banner rounded-t-lg object-fill">
-<<<<<<< HEAD
-                    <img class ="rounded-t-lg " src="images/Banner1.jpg" alt="banner">
-                </div>
-                <div class="post-Profile flex items-center gap-2 p-2">
-                    <img class="post-profilePicture object-fill h-20 w-20  rounded-full" src="images/Banner1.jpg" alt="profile picture">
-                    <a class="post-Username" href="">RobertoSaviano14</a>
-                    <a class="post-clubTag rounded-full bg-black p-0.5 px-1 opacity-30" href="">U-Club</a>
-=======
                     <img class ="rounded-t-lg " src="images/try/Banner1.jpg" alt="banner">
                 </div>
                 <div class="post-Profile flex items-center gap-2 p-2">
                     <img class="post-profilePicture object-fill h-20 w-20  rounded-full" src="images/try/100x100.jpg" alt="profile picture">
                     <a class="post-Username" href="">{{$post->clubberUsername}}</a>
                     <a class="post-clubTag rounded-full bg-black p-0.5 px-1 opacity-30" href="">{{$post->clubUsername}}</a>
->>>>>>> ce362b7 (add dinamic tags)
                 </div>
                     
                 <div class="post-info p-2">
@@ -74,7 +61,7 @@
 
                     <div class="likes-interaction items-center gap-1 flex"> 
                         <p>0</p>
-                        <button><i class="uil uil-heart" alt="like"></i></button>
+                        <button><i class="uil uil-heart" alt="like" ></i></button>
                     </div>
                     <div><button><i class="uil uil-comment"></i></button></div>
                     <div><button><i class="uil uil-tag"></i></button></div>
