87a0c8e60f743cc4bfb10cb0752880f9ca20bf67
Luca Ferar <luca.ferartofan@studio.unibo.it>
2023-02-02 15:59:40 +0100

    added buttons for interaction with posts

--- resources/views/Feed/feed.blade.php
+++ resources/views/Feed/feed.blade.php
@@ -30,7 +30,7 @@
             </div>            
     </nav>
 </header>
-<body class="bg-fixed ..." style="background-image: url(images/feed/background.jpg)">
+<body class="bg-fixed object-fill" style="background-image: url(images/feed/background.jpg)">
 <div class="py-[5%] lg:grid grid-cols-3 justify-between text-slate-200" >
     <div class="left"></div>
 
@@ -45,10 +45,10 @@
         <div class="w-21 items-center text-slate-200 py-2">
             <div class="post-User rounded-xl bg-black bg-opacity-50 backdrop-blur">
                 <div class ="post-banner rounded-t-lg object-fill">
-                    <img class ="rounded-t-lg " src="img/Banner1.jpg" alt="banner">
+                    <img class ="rounded-t-lg " src="images/Banner1.jpg" alt="banner">
                 </div>
                 <div class="post-Profile flex items-center gap-2 p-2">
-                    <img class="post-profilePicture object-fill h-20 w-20  rounded-full" src="img/profilepic.jpeg" alt="profile picture">
+                    <img class="post-profilePicture object-fill h-20 w-20  rounded-full" src="images/Banner1.jpg" alt="profile picture">
                     <a class="post-Username" href="">RobertoSaviano14</a>
                     <a class="post-clubTag rounded-full bg-black p-0.5 px-1 opacity-30" href="">U-Club</a>
                 </div>
@@ -56,10 +56,16 @@
                 <div class="post-info p-2">
                     <p class="post-caption">questo è un post molto lungo ceh se scrivi una descrizione cosi lunga sei uno stronzo</p>
                 </div>
-                <div class="post-buttons flex p-2">
+                <div class="post-buttons flex gap-2 p-2">
                     <!-- <div class="p-1"><a href=""><img class="h-5 w-5" src="img/like-not-pressed.png" alt="like"></a></div> -->
 
-                    <i class="uil uil-heart text-size-10" alt="like"></i>
+                    <div class="likes-interaction items-center gap-1 flex"> 
+                        <p>0</p>
+                        <button><i class="uil uil-heart" alt="like"></i></button>
+                    </div>
+                    <div><button><i class="uil uil-comment"></i></button></div>
+                    <div><button><i class="uil uil-tag"></i></button></div>
+                   
                 </div>
                     
             </div>
