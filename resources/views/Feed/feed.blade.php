6c627dae0f92dc4e44240b5e80c15e95d5d47b9b
Luca Ferar <luca.ferartofan@studio.unibo.it>
2023-02-02 15:09:34 +0100

    added full-responsive navbar on feed

--- resources/views/Feed/feed.blade.php
+++ resources/views/Feed/feed.blade.php
@@ -4,33 +4,36 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Post</title>
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+        <title>Clubbers</title>
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         @vite(['../resources/css/userPost.css', '../resources/js/Like/Like.js',  '../resources/js/app.js'])
     </head>
-    <h1>Home Feed</h1>
-    <h1>{{ Auth::user()->username}}</h1>
-    <h1><strong><a href="/logout">LOGOUT</a></strong></h1>
-    <h1>{{ Auth::user()->id}}</h1>
     
+
+<body class="bg-fixed ..." style="background-image: url(images/feed/background.jpg)">
     <!--NAVBAR-->
     <nav class="items-center px-10 py-5 bg-black backdrop-blur bg-opacity-40 text-slate-200 shadow-xl">
             <div class="flex items-center justify-between">
                 <div class="navbar-logo items-center flex gap-2">
-                    <img class="h-12 w-12 shadow-xl" src="img/ClubbersLogo.png" alt="Clubbers">
+                    <img class="h-12 w-12 shadow-xl" src="images/feed/ClubbersLogo.png" alt="Clubbers">
                     <h1 class="invisible md:visible lg:visible">Clubbers</h1>
                 </div>
-                <div class="navbar-search-bar flex gap-2">
-                    <input type="search" placeholder="connect with people...">
-                    <i><img src="" alt="search-alt"></i>
+                <div class="navbar-search-bar flex gap-4 p-2 bg-black backdrop-blur bg-opacity-40 rounded-2xl">
+                    <button><i class="uil uil-search p-3 rounded-full hover:bg-white hover:bg-opacity-20"></i></button>
+                    <input type="search" class="bg-black lg:px-20 py-2 rounded-full placeholder:text-center shadow-2xl" placeholder="connect with people...">
                 </div>
-                <div class="navbar-options items-center md:flex lg:flex gap-2">
-                    <a href="" class="bg-black bg-opacity-30 px-3 py-1 rounded-full hover:bg-opacity-20 hover:bg-white">create</a>
+                <div class="navbar-options items-center md:flex lg:flex lg:gap-4 gap-2">
                     <a href=""><img src="" class="bg-black bg-opacity-30 px-3 py-1 rounded-full hover:bg-opacity-20 hover:bg-white" alt="notification"></a>
-                    <a href=""><img src="img/shrek.jpg" class="rounded-full h-12 w-12 shadow-xl" alt="profile-picture"></a>
+                    <h1 class="invisible lg:visible">{{ Auth::user()->username}}</h1>
+                    <a href=""><img src="" class="rounded-full h-12 w-12 shadow-xl" alt="profile-picture"></a>
                 </div>
             </div>            
-        </nav>
+    </nav>
+
+
+</body>
+    
 
 
 
@@ -38,6 +41,14 @@
 
 
 
+
+
+
+
+    <!-- <h1>Home Feed</h1>
+    <h1>{{ Auth::user()->username}}</h1>
+    <h1><strong><a href="/logout">LOGOUT</a></strong></h1>
+    <h1>{{ Auth::user()->id}}</h1>
 
     @php
         $posts = App\Http\Controllers\postClubberController::getAll();  
@@ -52,7 +63,7 @@
             <h6>Questo post ha <div class='inline' id="likeNumber{{$post->id}}"></div> likes</h6>
             <button id="like-button" name="{{$post->id}}">METTI LIKE</button>
         </div>
-    @endforeach
+    @endforeach -->
 
 
 </html>
