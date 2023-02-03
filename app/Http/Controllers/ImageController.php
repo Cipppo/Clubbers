e973083ce22979176414a8ddd45a3714829b3d43
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-02-03 15:04:03 +0100

    Implemented getAlt

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,6 +8,7 @@ use App\Models\userProPic;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ImageController extends Controller
 {
@@ -27,7 +28,13 @@ class ImageController extends Controller
         }
     }
 
-    public function getProPic($username){
+    public static function getProPic($username){
+        $url = DB::table('user_pro_pic')->where('username', $username)->first()->URL;
+        return $url;
+    }
 
+    public static function getProPicAlt($username){
+        $alt = DB::table('user_pro_pic')->where('username', $username)->first()->alt;
+        return $alt;
     }
 }
