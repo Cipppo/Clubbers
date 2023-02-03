4fe598db9c914f857792d18271dbc97ed81af2a4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-02-03 13:11:14 +0100

    Add storeImage method

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,19 +4,30 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Image;
+use App\Models\userProPic;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    public const IMAGE_LOCAL_STORAGE_PATH = 'public/uploads/imgs/';
+    public static function storeProPic(Request $request){
+        $IMAGE_UPLOAD_URL = 'images/proPics/';
 
-    public function store(){
-        //MAYBE GONNA IMPLEMENT IT LATER
+        $file = $request->hasfile('fileIn');
+        if($file){
+            $newFile = $request->file('fileIn')->getClientOriginalName();
+            $request->file('fileIn')->move(public_path($IMAGE_UPLOAD_URL), $newFile);
+            $userPic = userProPic::create([
+                'username' => $request->username,
+                'URL' => $IMAGE_UPLOAD_URL.$newFile,
+                'alt' => $request->username." Profile Avatar",
+            ]);
+            $userPic->save();
+        }
     }
 
-    public function get(int $id){
+    public function getProPic($username){
 
     }
 }
