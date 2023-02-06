4095c03c0f7da9890b9911a470f8fd7835363881
Luca Ferar <luca.ferartofan@studio.unibo.it>
2023-02-06 10:54:51 +0100

    Solved conflict

--- resources/views/Feed/feed.blade.php
+++ resources/views/Feed/feed.blade.php
@@ -32,6 +32,51 @@
     </nav>
 </header>
 <body class="bg-fixed object-fill" style="background-image: url(images/feed/background.jpg)">
+<div class="py-[5%] lg:grid grid-cols-3 justify-between text-slate-200" >
+    <div class="left"></div>
+
+    <div class="feed px-3">
+
+    @php
+        $posts = App\Http\Controllers\postClubberController::getAll();  
+    @endphp
+
+
+    @foreach ($posts as $post)
+        <div class="w-21 items-center text-slate-200 py-2">
+            <div class="post-User rounded-xl bg-black bg-opacity-50 backdrop-blur">
+                <div class ="post-banner rounded-t-lg object-fill">
+                    <img class ="rounded-t-lg " src="images/try/Banner1.jpg" alt="banner">
+                </div>
+                <div class="post-Profile flex items-center gap-2 p-2">
+                    <img class="post-profilePicture object-fill h-20 w-20  rounded-full" src="{{App\Http\Controllers\ImageController::getProPic($post->clubberUsername)}}" alt="{{App\Http\Controllers\ImageController::getProPicAlt($post->clubberUsername)}}">
+                    <a class="post-Username" href="">{{$post->clubberUsername}}</a>
+                    <a class="post-clubTag rounded-full bg-black p-0.5 px-1 opacity-30" href="">{{$post->clubUsername}}</a>
+                </div>
+                    
+                <div class="post-info p-2">
+                    <p class="post-caption">{{$post->caption}}</p>
+                </div>
+                <div class="post-buttons flex gap-2 p-2">
+                    <!-- <div class="p-1"><a href=""><img class="h-5 w-5" src="img/like-not-pressed.png" alt="like"></a></div> -->
+
+                    <div class="likes-interaction items-center gap-1 flex"> 
+                        <p id="likeNumber{{$post->id}}">0</p>
+                        <button id="like-button" name="{{$post->id}}"><i class="uil uil-heart" alt="like"></i></button>
+                    </div>
+                    <div class="navbar-search-bar flex gap-4 p-2 bg-black backdrop-blur bg-opacity-40 rounded-2xl">
+                        <button><i class="uil uil-search p-3 rounded-full hover:bg-white hover:bg-opacity-20"></i></button>
+                        <input type="search" class="bg-black lg:px-20 py-2 rounded-full placeholder:text-center shadow-2xl" placeholder="connect with people...">
+                    </div>
+                    <div class="navbar-options lg:flex lg:gap-4 gap-2 items-center">
+                        <a href=""><img src="" class="bg-black bg-opacity-30 px-3 py-1 rounded-full hover:bg-opacity-20 hover:bg-white" alt="notification"></a>
+                        <div class="invisible lg:visible"><h1>{{ Auth::user()->username}}</h1></div>
+                        <a href=""><img src="" class="rounded-full h-12 w-12 shadow-xl" alt="profile-picture"></a>
+                    </div>
+                </div>            
+        </nav>
+    </header>
+<body class="bg-fixed object-fill" style="background-image: url(images/feed/background.jpg)">
 
     <div class="py-36 md:py-36 lg:py-24 lg:grid grid-cols-3 justify-between text-slate-200" >
 
@@ -71,7 +116,9 @@
                     </div>
                         
                 </div>
+                </div>           
             </div>
+                </div>           
         @endforeach
         </div>
 
