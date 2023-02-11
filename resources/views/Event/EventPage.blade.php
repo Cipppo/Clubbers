aad07feb2d72c99d77e600c5aa5e32256f8f18e6
Luca Ferar <luca.ferartofan@studio.unibo.it>
2023-02-11 19:26:53 +0100

    added user page info

--- resources/views/Event/EventPage.blade.php
+++ resources/views/Event/EventPage.blade.php
@@ -59,16 +59,19 @@
         <div class="">
           <img class="rounded-t-xl" src="{{url(App\Http\Controllers\ImageController::getBannerUrl($event->id))}}" alt="{{url(App\Http\Controllers\ImageController::getBannerAlt($event->id))}}"/>
           <div class="event-name">
+            <div class="event-info flex justify-between p-2">
             <h1 class="pl-5 p-2 text-5xl font-bold">{{$event->name}}</h1>
+            <div>
+            <p class="text-2xl">{{$event->Date}}</p>
+            <div class="flex items-center pl-[65%]">
+              <i class="uil uil-clock"></i>
+              <p class="text-2xl">{{$event->Time}}</p>
+            </div>
+            </div>
+            </div>
+            
             <div class="event-info px-5 py-2">
-              <div
-                class="event-date flex text-center justify-between font-bold">
-                <p class="text-2xl">{{$event->Date}}</p>
-                <div class="flex items-center">
-                  <i class="uil uil-clock"></i>
-                  <p class="text-2xl">{{$event->Time}}</p>
-                </div>
-              </div>
+              <div class="event-date flex text-center justify-between font-bold"></div>
               <div class="join-event-button">
                 <button class="p-2 bg-green-500 rounded-xl my-2 font-semibold hover:bg-green-700 hover:text-slate-300 shadow-lg">JOIN EVENT!</button>
               </div>
