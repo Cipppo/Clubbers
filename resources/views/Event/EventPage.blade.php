8029ae84f1f361e92d372142ac38dd628173373c
Luca Ferar <luca.ferartofan@studio.unibo.it>
2023-02-11 16:27:27 +0100

    reposition of the button on event page

--- resources/views/Event/EventPage.blade.php
+++ resources/views/Event/EventPage.blade.php
@@ -9,56 +9,27 @@
     <title>{{$event->name}}</title>
     @vite(['../resources/css/event.css'])
   </head>
-  <body
-    class="bg-fixed object-fill"
-    style="background-image: url({{url('images/feed/background2luce.jpg')}})"
-  >
+  <body class="bg-fixed object-fill" style="background-image: url({{url('images/feed/background2luce.jpg')}})">
     <!--NAVBAR-->
-    <nav
-      class="z-10 items-center w-full sticky-top fixed px-10 py-5 bg-black backdrop-blur bg-opacity-40 text-slate-200 shadow-xl"
-    >
-      <div class="flex items-center justify-between">
-        <div class="navbar-logo items-center flex gap-2">
-          <img
-            class="h-12 w-12 shadow-xl"
-            src=""
-            alt="Clubbers"
-          />
-          <a href="/home"><h2 class="invisible md:visible lg:visible">Clubbers</h2></a>
-        </div>
-        <div
-          class="navbar-search-bar flex gap-4 p-2 bg-black backdrop-blur bg-opacity-40 rounded-2xl"
-        >
-          <button>
-            <i
-              class="uil uil-search p-3 rounded-full hover:bg-white hover:bg-opacity-20"
-            ></i>
-          </button>
-          <input
-            type="search"
-            class="bg-black lg:px-20 py-2 rounded-full placeholder:text-center shadow-2xl"
-            placeholder="connect with people..."
-          />
-        </div>
-        <div class="navbar-options items-center md:flex lg:flex lg:gap-4 gap-2">
-          <a href=""
-            ><img
-              src=""
-              class="bg-black bg-opacity-30 px-3 py-1 rounded-full hover:bg-opacity-20 hover:bg-white"
-              alt="notification"
-          /></a>
-          <h2 class="invisible lg:visible">{{Auth::user()->username}}</h2>
-          <a href=""
-            ><img
-              src="{{url(App\Http\Controllers\ImageController::getProPic(Auth::user()->username))}}"
-              class="rounded-full h-12 w-12 shadow-xl"
-              alt=""
-          /></a>
-          <h2>
-            <a href="/logout"><strong>LOGOUT</strong></a>
-          </h2>
-        </div>
-      </div>
+    <nav class="z-10 items-center w-full sticky-top fixed px-10 py-5 bg-black backdrop-blur bg-opacity-40 text-slate-200 shadow-xl">
+            <div class="flex items-center justify-between">
+                <div class="navbar-logo items-center flex gap-2">
+                    <a href="/home" class="flex items-center">
+                        <img class="h-12 w-12 shadow-xl" src="../../images/feed/ClubbersLogo.png" alt="Clubbers">
+                        <h2 class="invisible md:visible lg:visible">Clubbers</h2>
+                    </a>
+                </div>
+                <div class="navbar-search-bar flex gap-4 p-2 bg-black backdrop-blur bg-opacity-40 rounded-2xl">
+                    <button><i class="uil uil-search p-3 rounded-full hover:bg-white hover:bg-opacity-20"></i></button>
+                    <input type="search" class="bg-black lg:px-20 py-2 rounded-full placeholder:text-center shadow-2xl" placeholder="connect with people...">
+                </div>
+                <div class="navbar-options items-center md:flex lg:flex lg:gap-4 gap-2">
+                    <a href=""><img src="" class="bg-black bg-opacity-30 px-3 py-1 rounded-full hover:bg-opacity-20 hover:bg-white" alt="notification"></a>
+                    <h2 class="invisible lg:visible">{{ Auth::user()->username}}</h2>
+                    <a href=""><img src="{{App\Http\Controllers\ImageController::getProPic(Auth::user()->username)}}" class="rounded-full h-12 w-12 shadow-xl" alt="{{App\Http\Controllers\ImageController::getProPicAlt(Auth::user()->username)}}"></a>
+                    <h2><a href="/logout"><strong>LOGOUT</strong></a></h2>
+                </div>
+            </div>            
     </nav>
 
     <!-- BODY -->
@@ -95,19 +66,14 @@
                   <p class="text-2xl">{{$event->Time}}</p>
                 </div>
               </div>
+              <div class="join-event-button">
+                <button class="p-3 bg-green-500 rounded-xl my-2 font-semibold hover:bg-green-700 shadow-lg">JOIN EVENT!</button>
+              </div>
               <p class="py-2">
                 {{$event->description}}
               </p>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="join-event-button text-center">
-        <div class="fixed p-3 top-34 w-[32%] items-center flex backdrop-blur bg-black bg-opacity-40 m-3 rounded-xl hover:bg-white hover:bg-opacity-20">
-            <div class="pl-[45%] rounded-xl text-center">
-                <a href="#">JOIN EVENT</a>
-            </div>
-            
         </div>
       </div>
     </div>
