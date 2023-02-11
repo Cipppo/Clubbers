e75e4fa18ac82f9f3c4b41ee2048c0d532327732
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-02-11 16:38:38 +0100

    refactor

--- resources/views/Feed/feed.blade.php
+++ resources/views/Feed/feed.blade.php
@@ -162,7 +162,7 @@
                         @foreach($events as $event)
                         <a href="/event/show/{{$event->id}}">
                             <div class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-103  duration-300">
-                                <div class="events-bg-img bg-cover w-full h-24 rounded-xl" style="background-image: url({{App\Http\Controllers\ImageController::getBannerUrl($event->id)}})">
+                                <div class="events-bg-img bg-cover w-full h-24 rounded-xl" style="ground-image: url({{App\Http\Controllers\ImageController::getBannerUrl($event->id)}})back">
                                     <div class="hover:bg-black hover:bg-opacity-20 hover:backdrop-blur-sm rounded-xl hover:delay-200">
                                         <div class="event-real rounded-xl h-24 bg-black bg-opacity-60 p-3 items-center">
                                             <div class="text-center flex justify-center">
