d9fb358076294df70ba3eb159f07027b3dc02b89
Luca Ferar <luca.ferartofan@studio.unibo.it>
2023-02-13 20:10:31 +0100

    fixed footer and menu

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -8,13 +8,13 @@
     </head>
     
     <body class="Back h-screen text-white">
-        <header class="bg-black bg-opacity-70">
+        <header class="bg-black bg-opacity-40 backdrop-blur-md p-2">
             <nav class="flex justify-between items-center w-[92%]  mx-auto">
                 <div>
                     <img class="w-16 cursor-pointer" src="DAFARE" alt="logo clubbers">
                 </div>
                 <div
-                    class="nav-links duration-500 bg-opacity-70 bg-black md:static absolute rounded-lg md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+                    class="nav-links duration-500 bg-black bg-opacity-40 md:bg-opacity-0 lg:bg-opacity-0 md:static absolute rounded-lg md:min-h-fit min-h-[60vh] left-0 top-[-9000%] md:w-auto  w-full flex items-center px-5">
                     <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                         <li>
                             <a class="hover:text-gray-500" href="#">Home</a>
@@ -40,10 +40,10 @@
     
     </body>
 
-    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-opacity-70 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-black dark:bg-opacity-70 dark:border-black">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">Clubbers™</a>. All Rights Reserved.
+    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-opacity-40 bg-black shadow md:flex md:items-center md:justify-between md:p-6 backdrop-blur-md">
+    <span class="text-sm sm:text-centertext-gray-400">© 2023 <a href="#" class="hover:underline">Clubbers™</a>. All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul class="flex flex-wrap items-center mt-3 text-smtext-gray-400 sm:mt-0">
         <li>
             <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
         </li>
@@ -64,7 +64,7 @@
             const navLinks = document.querySelector('.nav-links')
             function onToggleMenu(e){
                 e.name = e.name === 'menu' ? 'close' : 'menu'
-                navLinks.classList.toggle('top-[9%]')
+                navLinks.classList.toggle('top-[110%]')
             }
         </script>
 
