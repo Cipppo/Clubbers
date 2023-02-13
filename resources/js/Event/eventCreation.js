4ea2e0d9107113201b66254d47550d63bbe522e3
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-02-13 10:56:43 +0100

    Now validating date

--- resources/js/Event/eventCreation.js
+++ resources/js/Event/eventCreation.js
@@ -30,13 +30,53 @@ longDesc.bind('input propertychange', function(e){
     counter2.html(`${this.value.length}/150`)
 });
 
+function validateDate(date){
+    // Date format: YYYY-MM-DD
+    var datePattern = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
 
+    // Check if the date string format is a match
+    var matchArray = date.match(datePattern);
+    if (matchArray == null) {
+        return false;
+    }
+
+    // Remove any non digit characters
+    var dateString = date.replace(/\D/g, ''); 
+
+    // Parse integer values from the date string
+    var year = parseInt(dateString.substr(0, 4));
+    var month = parseInt(dateString.substr(4, 2));
+    var day = parseInt(dateString.substr(6, 2));
+   
+    // Define the number of days per month
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // Leap years
+    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+        daysInMonth[1] = 29;
+    }
+
+    if (month < 1 || month > 12 || day < 1 || day > daysInMonth[month - 1]) {
+        return false;
+    }
+    return true;
+
+}
+
+$('#eventDay').on('input', function(e){
+    console.log($("#eventDay").val());
+    if(validateDate($("#eventDay").val()) == true){
+        $("#partFive").css('display', 'block');
+    }else{
+        $("#partFive").css('display', 'none');
+    }
+});
 
 function displayAll(){
     $("#partTwo").css('display', 'block');
     $("#partThree").css('display', 'block');
     $("#partFour").css('display', 'block');
-    $("#partFive").css('display', 'block');
+    //PART 5 later
     
 }
 
