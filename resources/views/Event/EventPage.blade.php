35aae71740b320257f7dfa2f6fea3bb5450f14e9
Luca Ferar <luca.ferartofan@studio.unibo.it>
2023-02-13 11:32:34 +0100

    added user posts refered to event

--- resources/views/Event/EventPage.blade.php
+++ resources/views/Event/EventPage.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"/>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{$event->name}}</title>
-    @vite(['../resources/css/event.css', '../resources/js/app.js', '../resources/js/Event/event.js'])
-  </head>
-  <body class="bg-fixed object-fill" style="background-image: url({{url('images/feed/background2luce.jpg')}})">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"/>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{$event->name}}</title>
+        @vite(['../resources/css/event.css', '../resources/js/app.js', '../resources/js/Event/event.js'])
+    </head>
+    <body class="bg-fixed object-fill" style="background-image: url({{url('images/feed/background2luce.jpg')}})">
     <!--NAVBAR-->
-    <nav class="z-10 items-center w-full sticky-top fixed px-10 py-5 bg-black backdrop-blur bg-opacity-40 text-slate-200 shadow-xl">
+        <nav class="z-10 items-center w-full sticky-top fixed px-10 py-5 bg-black backdrop-blur bg-opacity-40 text-slate-200 shadow-xl">
             <div class="flex items-center justify-between">
                 <div class="navbar-logo items-center flex gap-2">
                     <a href="/home" class="flex items-center">
@@ -30,61 +30,79 @@
                     <h2><a href="/logout"><strong>LOGOUT</strong></a></h2>
                 </div>
             </div>            
-    </nav>
+        </nav>
 
     <!-- BODY -->
-    <div
-      class="py-36 md:py-36 lg:py-24 lg:grid grid-cols-3 justify-between text-slate-200"
-    >
-      <div class="club-info">
-        <div
-          class="fixed w-[32%] p-3 top-34 flex items-center backdrop-blur bg-black bg-opacity-40 m-3 rounded-xl"
-        >
-          <div class="profile-pic-club">
-            <img
-              src="{{url(App\Http\Controllers\ImageController::getProPic($event->clubName))}}"
-              class="h-20 w-20 rounded-full"
-              alt="{{url(App\Http\Controllers\ImageController::getProPicAlt($event->clubName))}}"
-            />
+    <div class="py-36 md:py-36 lg:py-24 lg:grid grid-cols-3 justify-between text-slate-200">
+        <div class="club-info">
+            <div class="fixed w-[32%] p-3 top-34 flex items-center backdrop-blur bg-black bg-opacity-40 m-3 rounded-xl">
+            <div class="profile-pic-club">
+              <img src="{{url(App\Http\Controllers\ImageController::getProPic($event->clubName))}}" class="h-20 w-20 rounded-full"
+                alt="{{url(App\Http\Controllers\ImageController::getProPicAlt($event->clubName))}}"/>
+            </div>
+            <div class="name-club pl-8">
+              <a href="#">{{$event->clubName}}</a>
+              <p>{{App\Http\Controllers\Usercontroller::getAddress($event->clubName)}}</p>
+            </div>
           </div>
-          <div class="name-club pl-8">
-            <a href="#">{{$event->clubName}}</a>
-            <p>{{App\Http\Controllers\Usercontroller::getAddress($event->clubName)}}</p>
-        </div>
-        </div>
       </div>
-      <div
-        class="event-real mt-4 rounded-xl bg-black backdrop-blur bg-opacity-40 text-slate-200 shadow-xl h-full"
-      >
+
+      <div class="event-real mt-4 rounded-xl bg-black backdrop-blur bg-opacity-40 text-slate-200 shadow-xl h-full">
         <div class="">
           <img class="rounded-t-xl" src="{{url(App\Http\Controllers\ImageController::getBannerUrl($event->id))}}" alt="{{url(App\Http\Controllers\ImageController::getBannerAlt($event->id))}}"/>
           <div class="event-name">
             <div class="event-info flex justify-between p-2">
-            <h1 class="pl-5 p-2 text-5xl font-bold">{{$event->name}}</h1>
+              <h1 class="pl-5 p-2 text-5xl font-bold">{{$event->name}}</h1>
             <div>
             <p class="text-2xl">{{$event->Date}}</p>
             <div class="flex items-center pl-[65%]">
               <i class="uil uil-clock"></i>
               <p class="text-2xl">{{$event->Time}}</p>
             </div>
-            </div>
-            </div>
-            
-            <div class="event-info px-5 py-2">
-              <div class="event-date flex text-center justify-between font-bold"></div>
-              @if($event->onGoing == "True" && Auth::user()->type == "User")
-              <div class="join-event-button">
-                <button id="partecipateButton" class=""></button>
-              </div>
-              @endif
-              <p class="py-2">
-                {{$event->description}}
-              </p>
-            </div>
+          </div>
+        </div>
+        <div class="event-info px-5 py-2">
+          <div class="event-date flex text-center justify-between font-bold"></div>
+          @if($event->onGoing == "True" && Auth::user()->type == "User")
+          <div class="join-event-button">
+            <button id="partecipateButton" class=""></button>
+          </div>
+          @endif
+          <p class="py-2">{{$event->description}}</p>
           </div>
         </div>
       </div>
     </div>
+    <div class="">
+      <a href="">
+          <div class="w-21 items-center text-slate-200 py-2">
+              <div class="post-User rounded-xl bg-black bg-opacity-50 backdrop-blur">
+                  <div class ="post-banner rounded-t-lg object-fill">
+                      <a href="  "><img class ="rounded-t-lg " src="banner " alt=""></a>
+                  </div>
+                  <div class="post-Profile flex items-center gap-2 p-2">
+                    <img class="post-profilePicture object-fill h-20 w-20  rounded-full" src=" " alt=" foto profilo">
+                    <a class="post-Username" href="">clubberUsername</a>
+                    <a class="post-clubTag rounded-full bg-black p-0.5 px-1 opacity-30" href="">clubUsername</a>
+                  </div>
+                  <div class="post-info p-2">
+                      <p class="post-caption">caption</p>
+                  </div>
+                  <div class="post-buttons flex gap-2 p-2">
+                  <!-- <div class="p-1"><a href=""><img class="h-5 w-5" src="img/like-not-pressed.png" alt="like"></a></div> -->
+                  <div class="likes-interaction items-center gap-1 flex"> 
+                      <p id="likeNumber">0</p>
+                      <button id="like-button" name=""><i class="uil uil-heart" id="likeIcon"></i></button{$postElement->id}}>
+                  </div>
+                  <div><button><i class="uil uil-comment" ></i></button></div>
+                  <div><button><i class="uil uil-tag"></i></button></div>
+              </div>
+          </div>
+      </a>
+        
     </div>
-  </body>
+  </div>
+</div>
+</div>
+</body>
 </html>
