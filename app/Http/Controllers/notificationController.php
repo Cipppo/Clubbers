013bcc42c6565844ddd0c068902fad3d4921b8b4
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-02-13 22:30:22 +0100

    Update

--- app/Http/Controllers/notificationController.php
+++ app/Http/Controllers/notificationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class notificationController extends Controller
 {
@@ -10,6 +11,14 @@ class notificationController extends Controller
 
     public static function show(){
         return view('notification.notifications');
+    }
+
+    public static function areThereNotification(){
+        if(count(Auth::user()->unreadNotifications) > 0){
+            return 1;
+        }else{
+            return 0;
+        }
     }
 
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
 import './bootstrap';
+import $ from 'jquery';
+
+
+
+
+
+
+let notificationBell = $("#notificationBell");
+
+
+
+
+
+function updateBell(){
+    $.get('/notification/present').then(
+        response => {
+            if(response == 0){
+                notificationBell.css('color', 'white');
+            }else{
+                notificationBell.css('color', 'red');
+            }
+        }
+    )
+}
+
+updateBell()
